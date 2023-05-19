0aba86fb1ff20732340caca9147ac36da9f10839
Graham G. Hughes <graham@forthstreet.com>
2023-05-19 20:00:19 +0100

    Center footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,13 +8,13 @@ export default function Footer() {
     bg-header-secondary text-white dark:text-stone-300 text-sm"
     >
       <div className="flex items-center">
-        <div>
+        <div className="text-center md:text-left">
           <span className="whitespace-nowrap">&copy; 2021-2023 Forth Street Limited.</span>{" "}
           <span className="whitespace-nowrap">Company no: 10487304. Registered in England.</span>{" "}
           <span className="whitespace-nowrap">VAT no: 256 6843 69.</span>
         </div>
       </div>
-      <div className="md:text-right text-3xl">
+      <div className="text-center md:text-right text-3xl">
         <a
           href="https://uk.linkedin.com/in/grahamghughes"
           target="_blank"
